ffd4c1aab9e78ded9bc98ca55ac3f637355fe5e7
nathanmiguel123 <nathan.cm77@gmail.com>
2020-01-28 16:31:43 -0300

    update.config modified

--- T_Sploit/T_Sploit/aboutForm.cs
+++ T_Sploit/T_Sploit/aboutForm.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Net;
+using System.Xml;
 
 namespace T_Sploit
 {
     public partial class aboutForm : UserControl
     {
+        WebClient wc = new WebClient();
         public aboutForm()
         {
             InitializeComponent();
@@ -34,6 +37,17 @@ namespace T_Sploit
         {
             string version = System.Configuration.ConfigurationManager.AppSettings["version"];
             int v = Int32.Parse(version);
+
+            Uri xmlUrl = new Uri("https://raw.githubusercontent.com/nathanmiguel123/TSploit/master/manifest/update.config");
+            wc.DownloadFileAsync(xmlUrl, "update.xml");
+
+            XmlDocument update_info = new XmlDocument();
+            update_info.Load("update.xml");
+
+            foreach (XmlNode ndoe in update_info.DocumentElement)
+            {
+
+            }
         }
     }
 }
